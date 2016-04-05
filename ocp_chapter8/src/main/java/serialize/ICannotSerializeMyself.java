a20672f8f5fbee91f3bc60d50b93d9ccd5f728a5
Wojciech Piotrowiak | Hybris <wojciech.piotrowiak@sap.com>
2016-04-05 22:58:15 +0200

    another changing from main method to test

--- ocp_chapter8/src/main/java/serialize/ICannotSerializeMyself.java
+++
@@ -1,3 +0,0 @@
-package serialize;
-public class ICannotSerializeMyself {
-}
--- ocp_chapter8/src/test/java/CloseYourStreamPlease.java
+++ ocp_chapter8/src/test/java/CloseYourStreamPlease.java
@@ -1,52 +1,55 @@
+import org.junit.Test;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 
 public class CloseYourStreamPlease {
 
-    public static void main(String... args)
+    @Test
+    public void rightTest()
     {
         for(int i=0;i<8000;i++) {
-            //unclosedUsage();
             rightUsage();
         }
     }
 
-    private static void unclosedUsage()
-    {
+    @Test(expected=FileNotFoundException.class)
+    public void unclosedTest() throws FileNotFoundException {
+        for(int i=0;i<8000;i++) {
+                FileInputStream f = new FileInputStream(getSmallfileUrl().getFile());
+                BufferedInputStream br=new BufferedInputStream(f);
+                FileOutputStream outputStream=new FileOutputStream("copyofsmalltextfile.txt");
+                BufferedOutputStream out=new BufferedOutputStream(outputStream);
+
+                int length;
+                byte[] buffer=new byte[1024];
+            try {
+                while ((length = br.read(buffer)) >0)
+                {
+                    out.write(buffer,0,length);
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void unclosedUsage() throws FileNotFoundException {
     //may happen: java.io.FileNotFoundException: smalltextfile.txt (Too many open files in system)
     // or memory resource problems
 
-        try
-        {
-            FileInputStream f = new FileInputStream("smalltextfile.txt");
-            BufferedInputStream br=new BufferedInputStream(f);
-            FileOutputStream outputStream=new FileOutputStream("copyofsmalltextfile.txt");
-            BufferedOutputStream out=new BufferedOutputStream(outputStream);
 
-            int length;
-            byte[] buffer=new byte[1024];
-            while ((length = br.read(buffer)) >0)
-            {
-                out.write(buffer,0,length);
-                out.flush();
-            }
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+
     }
-    private static void rightUsage()
+    private void rightUsage()
     {
-        try(FileInputStream f = new FileInputStream("smalltextfile.txt");
+        try(FileInputStream f = new FileInputStream(getSmallfileUrl().getFile());
             BufferedInputStream br=new BufferedInputStream(f);
             FileOutputStream outputStream=new FileOutputStream("copyofsmalltextfile.txt");
             BufferedOutputStream out=new BufferedOutputStream(outputStream))
@@ -67,5 +70,10 @@ public class CloseYourStreamPlease {
         {
             e.printStackTrace();
         }
+    }
+
+    private URL getSmallfileUrl() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return classLoader.getResource("smalltextfile.txt");
     }
 }
