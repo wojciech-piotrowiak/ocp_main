60588de95a4e6c89fdf153553763f8a20b44245c
Wojciech Piotrowiak | Hybris <wojciech.piotrowiak@sap.com>
2016-04-05 23:09:31 +0200

    test bug fix

--- ocp_chapter8/src/test/java/CloseYourStreamPlease.java
+++ ocp_chapter8/src/test/java/CloseYourStreamPlease.java
@@ -8,6 +8,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
+import static org.junit.Assert.fail;
+
 public class CloseYourStreamPlease {
 
     @Test
@@ -18,34 +20,20 @@ public class CloseYourStreamPlease {
         }
     }
 
-    @Test(expected=FileNotFoundException.class)
-    public void unclosedTest() throws FileNotFoundException {
+    @Test
+    public void unclosedTest()  {
+        //may happen: java.io.FileNotFoundException: smalltextfile.txt (Too many open files in system)
+        // or memory resource problems
+
         for(int i=0;i<8000;i++) {
+            try {
                 FileInputStream f = new FileInputStream(getSmallfileUrl().getFile());
                 BufferedInputStream br=new BufferedInputStream(f);
-                FileOutputStream outputStream=new FileOutputStream("copyofsmalltextfile.txt");
-                BufferedOutputStream out=new BufferedOutputStream(outputStream);
-
-                int length;
-                byte[] buffer=new byte[1024];
-            try {
-                while ((length = br.read(buffer)) >0)
-                {
-                    out.write(buffer,0,length);
-                    out.flush();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                fail("Too many open files in system");
             }
+
         }
-    }
-
-    private void unclosedUsage() throws FileNotFoundException {
-    //may happen: java.io.FileNotFoundException: smalltextfile.txt (Too many open files in system)
-    // or memory resource problems
-
-
-
     }
     private void rightUsage()
     {
