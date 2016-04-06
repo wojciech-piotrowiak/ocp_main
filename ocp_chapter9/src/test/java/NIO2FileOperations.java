ce5f310cdf5bf9a693bb230eb080e60c14f5f384
Wojciech Piotrowiak | Hybris <wojciech.piotrowiak@sap.com>
2016-04-06 23:04:02 +0200

    new Files test

---
+++ ocp_chapter9/src/test/java/NIO2FileOperations.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class NIO2FileOperations {
+
+    @Test
+    public void readAllLines() throws IOException {
+        final Path path = Paths.get(getFile().getFile());
+        final List<String> strings = Files.readAllLines(path);
+        Assert.assertEquals(3,strings.size());
+        Assert.assertEquals("1",strings.get(0));
+    }
+
+    @Test
+    public void file() throws IOException {
+        final Path path = Paths.get(getFile().getFile());
+
+        Assert.assertFalse(Files.isSymbolicLink(path));
+        Assert.assertTrue(Files.isReadable(path));
+        Assert.assertFalse(Files.isExecutable(path));
+        Assert.assertFalse(Files.isHidden(path));
+    }
+
+    private URL getFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return classLoader.getResource("lines.txt");
+    }
+}
