189c89a1d27795801e0b22635ecba9d6328515d2
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-04-10 10:27:59 +0200

    file search and walk operations

---
+++ ocp_chapter9/src/test/NIO2SearchAndWalk.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NIO2SearchAndWalk {
+
+    @Test
+    public void walk() throws IOException {
+        Path path = Paths.get(getFile("searchme"));
+        Assert.assertEquals(4,Files.walk(path).filter(p->p.toString().endsWith(".txt")).peek(System.out::println).count());
+    }
+
+    @Test
+    public void list() throws IOException {
+        Path path = Paths.get(getFile("searchme"));
+        Assert.assertEquals(3,Files.list(path).
+                map(p->p.toAbsolutePath()).peek(System.out::println).count());
+    }
+
+    private String getFile(String path) {
+        return getClass().getResource(path).getFile().substring(1);
+    }
+}
