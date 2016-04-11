efd26373b0c3412f2b97fbdcf25610a3caf503bd
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-04-11 12:17:12 +0200

    walk with depth example

--- ocp_chapter9/src/test/NIO2SearchAndWalk.java
+++ ocp_chapter9/src/test/NIO2SearchAndWalk.java
@@ -15,11 +15,20 @@ public class NIO2SearchAndWalk {
     }
 
     @Test
-    public void list() throws IOException {
+    public void walkWithDepth() throws IOException {
+        Path path = Paths.get(getFile("searchme"));
+        Assert.assertEquals(2,Files.walk(path,1).filter(p->p.toString().endsWith(".txt")).peek(System.out::println).count());
+    }
+
+    @Test
+    public void listFiles() throws IOException {
         Path path = Paths.get(getFile("searchme"));
         Assert.assertEquals(3,Files.list(path).
                 map(p->p.toAbsolutePath()).peek(System.out::println).count());
     }
+
+
+
 
     private String getFile(String path) {
         return getClass().getResource(path).getFile().substring(1);
