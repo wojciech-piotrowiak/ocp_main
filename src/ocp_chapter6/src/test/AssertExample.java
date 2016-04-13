774a5ec95c3080b0f0f1547a1c188862593e48d7
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-04-13 23:21:48 +0200

    assert example

--- src/ocp_chapter6/src/test/AssertExample.java
+++ src/ocp_chapter6/src/test/AssertExample.java
@@ -2,16 +2,18 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AssertExample {
-
-    @BeforeClass
-    public static void oneTimeSetUp()
-    {
-        ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
+//depednds on -ea -da /enable,disable flags in your java configuration
+    @Test
+    public void defaultAssertBehaviour() {
+        assert 3 < 2;
     }
 
     @Test(expected = AssertionError.class)
-    public void assertMe()
-    {
-        assert 3<2;
+    public void assertsEnabled() {
+        //magic trick to turn on assertions
+        ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
+        assert 3 < 2;
     }
+
+
 }
