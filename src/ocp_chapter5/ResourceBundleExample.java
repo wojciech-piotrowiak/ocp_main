82a93ed51cfd6085855ca78f0b3ce9dfcb90d30d
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-04-18 21:10:01 +0200

    fallback resource example + locale builder example

--- src/ocp_chapter5/ResourceBundleExample.java
+++ src/ocp_chapter5/ResourceBundleExample.java
@@ -36,4 +36,21 @@ public class ResourceBundleExample {
         ResourceBundle bundle = ResourceBundle.getBundle("chapter5//test", Locale.GERMAN);
         bundle.keySet().stream().forEach(System.out::println);
     }
+
+    @Test
+    public void builderExample() {
+        Locale customLocale = new Locale.Builder().setRegion("US").setLanguage("pl").build();
+        Assert.assertEquals("pl", customLocale.getLanguage());
+        Assert.assertEquals("US", customLocale.getCountry());
+    }
+
+    @Test
+    public void fallbackExample() {
+        //we do not have resources for custom locale, however more general resource is available
+        Locale customLocale = new Locale.Builder().setRegion("PL").setLanguage("DE").build();
+        ResourceBundle bundle = ResourceBundle.getBundle("chapter5//test", customLocale);
+        Assert.assertTrue(bundle.getKeys().hasMoreElements());
+    }
+
+
 }
