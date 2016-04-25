5529946dbd5df105fc32ecddcd53329cd92fb1de
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-04-25 23:02:54 +0200

    primitive stream examples

---
+++ src/ocp_chapter4/streams/PrimitiveStreamsExample.java
@@ -0,0 +1,27 @@
+package ocp_chapter4.streams;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public class PrimitiveStreamsExample {
+
+    @Test
+    public void doubleStreamTest() {
+        DoubleStream empty = DoubleStream.empty();
+        Assert.assertFalse(empty.average().isPresent());
+
+        DoubleStream doubleStream = DoubleStream.of(3.2);
+        double asDouble = doubleStream.findFirst().getAsDouble();
+        Assert.assertEquals(3.2, asDouble, 1);
+    }
+
+    @Test
+    public void rangeExamples() {
+        Assert.assertEquals(4, IntStream.range(1, 5).count());
+        Assert.assertEquals(5, IntStream.rangeClosed(1, 5).count());
+
+    }
+}
